a1d7eabfa7a834f98082facabec4e59490b35176
Vidya Adnina <vidya.adnina@gmail.com>
2025-06-25 14:03:35 +0700

    Update Shipment_Report.sql

--- Shipment_Report.sql
+++ Shipment_Report.sql
@@ -5,24 +5,33 @@ DROP TABLE IF EXISTS `shipment-report-q1.Data_q1.MMN_LMK`;
 CREATE TABLE `shipment-report-q1.Data_q1.MMN_LMK`
 AS
 (SELECT 
+    EXTRACT(MONTH from m.BL_Date) AS month, 
     m.PT, 
     m.Project_No, 
-    m.BL_Date, 
-    m.BL_Month, 
+    m.BL_Date,
     m.Source, 
     m.Enduser, 
     m.Owner, 
     m.Selling_Term, 
     m.HMA,
+    s._TA_LP AS TA_LP,
+    s.Comm_Loading AS Comm_LP,
+    s.Comp_Loading AS Comp_LP,
+    s.TA_DP,
+    s.Comm_Discharge AS Comm_DP,
+    s.Comp_Discharge AS Comp_DP,
+    s.Issued_Date_LP AS Issued_Cert_LP,
+    s.Surveyor_LP,
     m.Ni_LP, 
-    m.Ni_DP, 
     m.MC_LP, 
-    m.MC_DP, 
     m.SiO2_LP, 
-    m.SiO2_DP, 
     m.MgO_LP, 
-    m.MgO_DP, 
     m.Fe_LP, 
+    
+    m.Ni_DP, 
+    m.MC_DP, 
+    m.SiO2_DP,
+    m.MgO_DP, 
     m.Fe_DP,
     m.Qty_load_port AS QTY_LP, 
     m.Qty_Disch_Port AS QTY_DP, 
@@ -31,7 +40,7 @@ AS
     m.Harga_Kontrak_Vendor, 
     m.Detail_Harga_Vendor, 
     m.Acuan_Coa_Vendor,
-    m.HPM_Vendor_FINAL, 
+    m.HPM_Vendor_FINAL AS HPM_Vendor, 
     m.Buying_Price_ AS Buy_Price, 
     m.Perhitungan_Bonus_Ni_Vendor AS Bonus_Ni_Vendor,
     m.Perhitungan_Penalty_Ni_Vendor AS Penalty_Ni_Vendor,
@@ -41,12 +50,12 @@ AS
     m.Perhitungan_Penalty_Fe_Vendor AS Penalty_Fe_Vendor,
     m.Perhitungan_Penalty_MC_Vendor AS Penalty_MC_Vendor,
     m.Perhitungan_Penalty_SiO2_Vendor AS Penalty_SiO2_Vendor,
-    m.Bonus_QTY_Vendor__Marketing_fee AS Bonus_QTY_orMF_Vendor,
+    m.Bonus_QTY_Vendor__Marketing_fee AS QTY_or_MF_Vendor,
     m.Subtotal_Vendor_Nickel_only + Freight_Vendor + PPN_Freight_11 AS COGS,
     m.Harga_Kontrak_Enduser, 
     m.Detail_Harga_Enduser, 
     m.Acuan_Coa_Enduser,
-    m.HPM_Enduser_FINAL, 
+    m.HPM_Enduser_FINAL AS HPM_Enduser, 
     m.Buying_Price_ AS Sell_Price,
     m.Perhitungan_Bonus_Ni_Enduser AS Bonus_Ni_Enduser,
     m.Perhitungan_Penalty_Ni_Enduser AS Penalty_Ni_Enduser,
@@ -58,12 +67,15 @@ AS
     m.Perhitungan_Penalty_SiO2_Enduser AS Penalty_SiO2_Enduser,
     m.Bonus_QTY_Enduser AS Bonus_QTY_Enduser,
     m.Grand_Total_Enduser AS Revenue,
-    m.Nickel_Margin_USD AS Margin
-FROM `shipment-report-q1.Data_q1.margin` m
+    m.Nickel_Margin_USD AS Margin,
+    m.Ni_DP - m.Ni_LP as Ni_diff,
+    m.MC_DP - m.MC_LP as MC_diff,
+    HPM_Enduser - HPM_Vendor AS HPM_diff
+FROM `shipment-report-q1.Data_q1.margins` m
 JOIN `shipment-report-q1.Data_q1.shipsched` s
 ON m.Project_No = s.Project_No
-WHERE m.BL_Month IN ('January','February','March') AND m.Exclude_VAS = TRUE
+WHERE m.BL_Month IN ('January','February','March') AND m.Exclude_VAS = TRUE AND m.Done_Calculate_1 = TRUE
 ORDER BY m.Project_No
 );
 
-
+SELECT * FROM Data_q1.MMN_LMK
